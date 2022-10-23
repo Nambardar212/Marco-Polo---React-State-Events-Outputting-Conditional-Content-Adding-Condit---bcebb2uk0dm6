58d349a1e8a292b5088051acb0bbec406f3c41e3
Nambardar212 <nikhilsaini0210@gmail.com>
2022-10-24 02:05:28 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [text,setText] = useState(true)
   
+  const handleClick = () => {
+      setText(!text)
+  }
+
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{text === true ? 'Macro' : 'Polo'}</h1>
+      <button id="marco-polo-toggler" onClick={handleClick}>{text === true ? 'Polo' : 'Macro'}</button>
     </div>
   )
 }
