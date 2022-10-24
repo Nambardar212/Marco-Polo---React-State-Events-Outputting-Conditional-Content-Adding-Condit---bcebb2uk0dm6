5b2cb111f23a64ced815dea4c2a578530e4b7f64
Nambardar212 <nikhilsaini0210@gmail.com>
2022-10-24 23:59:50 +0530

    3rd

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,22 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const [text,setText] = useState('Macro')
   const [state, setState] = useState(true)
   
   const handleClick = () => {
     if(state === true){
       setState(false)
-      setText('Polo')
     }
     else {
        setState(true)
-       setText('Macro')
     }
       
   }
 
   return (
     <div id="main">
-      { state === true ? <h1 id="marco-polo">{text}</h1> : <h1 id="marco-polo">{text}</h1>}
-      <button id="marco-polo-toggler" onClick={handleClick}>{state === true ? 'Polo' : 'Macro'}</button>
+      { state === true ? <h1 id="marco-polo">Macro</h1> : <h1 id="marco-polo">Polo</h1>}
+      <button id="marco-polo-toggler" onClick={handleClick}>{state === true ? Polo : Macro}</button>
     </div>
   )
 }
